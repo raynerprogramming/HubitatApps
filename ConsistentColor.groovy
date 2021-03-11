5b742d22e23b2f3a2a526b5ffe7c1fc8df4984a8
m <m>
2021-03-10 21:09:15 -0600

    add polling

--- ConsistentColor.groovy
+++ ConsistentColor.groovy
@@ -40,6 +40,8 @@ def initialize() {
     subscribe(master, "colorTemperature", handleChangeEvent)
     subscribe(master, "switch", handleChangeEvent)
     unschedule()
+    atomicState.instanceCount = 0
+    log.debug "initialized state to: " +atomicState.instanceCount
 }
 
 def uninstalled() {
@@ -56,21 +58,61 @@ def handleDeviceEvent(evt) {
 }
 
 def handleChangeEvent(evt) {
+    atomicState.instanceCount = atomicState.instanceCount + 1
+    thisinstance = atomicState.instanceCount 
+    log.debug "Consistent Colors handleChangeEvent: " + thisinstance
     log.debug "handleChangeEvent"
     log.debug master
-    rgbDevices.each {
-        log.debug it
-        if (it.currentValue("switch") == "on"){
-            updateDevice(it)
+    tempDevices = rgbDevices
+    tempMaster = master
+    while(tempDevices){
+        if(thisinstance != atomicState.instanceCount){
+                log.debug "new instance detected, breaking instance: " + thisinstance
+                break;
+            }
+        log.debug "====================================================="
+        log.debug "Consistent Colors handleChangeEvent: " + thisinstance
+        log.debug "====================================================="
+        log.debug "temp devices " + tempDevices.size() + " " + tempDevices  
+        tempDevices.findAll{it.currentValue("switch") == "on"}.each {
+            log.debug "----------------------------------------------------------"
+            log.debug it            
+            if (it.currentValue("switch") == "on"){
+                if(deviceEqualMaster(it)){
+                    log.debug "removing device " + it
+                    tempDevices -= it
+                }else{
+                    updateDevice(it)
+                }            
+            }else{
+                log.debug "off"
+            }
         }
-    }    
+        pause(15000)
+        if(!deviceEqualMaster(tempMaster)){
+            log.debug "detected change to master, breaking current loop"
+            break
+        }
+    }
 }
-
+def deviceEqualMaster(device){    
+    currentLevel = device.currentValue("level", true)
+    currentColorMode = device.currentValue("colorMode", true)
+    currentColorTemperature = device.currentValue("colorTemperature", true)
+    currentHue = device.currentValue("hue", true)
+    currentSaturation = device.currentValue("saturation", true)
+    levelAndModeMatch = currentLevel == master.currentLevel && currentColorMode == master.currentColorMode
+    if(master.currentColorMode == 'CT'){
+        returnValue = levelAndModeMatch && (Math.abs(currentColorTemperature - master.currentColorTemperature)<15)
+    }else{
+        returnValue = levelAndModeMatch && currentHue == master.currentHue && currentSaturation == master.currentSaturation
+    }
+    log.debug "Comparing " + device + " and " + master + " equal: " + returnValue
+    log.debug "" +  device + ": " + " level: " + currentLevel + " mode: " + currentColorMode + " temperature: " + currentColorTemperature + " hue: " + currentHue + " saturation: " + currentSaturation
+    log.debug "" +  master + ": " + " level: " + master.currentLevel + " mode: " + master.currentColorMode + " temperature: " + master.currentColorTemperature + " hue: " + master.currentHue + " saturation: " + master.currentSaturation
+    return returnValue
+}
 def updateDevice(current) {
-    log.debug "update device " + current + " from " + master
-    log.debug "new color " + master.currentColor
-    log.debug "new colorTemperature " + master.currentColorTemperature
-    log.debug "new level " + master.currentLevel   
     current.setLevel(master.currentLevel)     
     if(master.currentColorMode == 'CT'){
         log.debug "new color mode CT"        
@@ -79,8 +121,4 @@ def updateDevice(current) {
         log.debug "new color mode RGB"
         current.setColor([hue:master.currentHue,saturation:master.currentSaturation,level:master.currentLevel]); 
     }
-    // log.debug "Consistent Colors: setting ${current.displayName} level to ${master.currentLevel}"
-    // log.debug "Consistent Colors: setting ${current.displayName} colorMode to ${master.currentColorMode}"
-    // log.debug "Consistent Colors: setting ${current.displayName} colorTemperature to ${master.currentColorTemperature}"
-    // log.debug "Consistent Colors: setting ${current.displayName} color to ${master.currentColor}"    
 }
